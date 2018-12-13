10fa9a661ef050f13380895db2d4c6073d357f3b
youngdu <youngdu@dayeteng.com>
2018-12-13 18:43:17 +0800

    柱状图组件效果
    Signed-off-by: youngdu <youngdu@dayeteng.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Timer from './components/timer/timer';
+import Graph from './components/graph/graph';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Timer />
+        <Graph />
       </div>
     );
   }
